d40833e534282d91655ac986f11026e9311205a0
Eivind Uggedal <eu@redflavor.com>
2008-03-25 20:54:08 +0100

    Adding base roert module.

---
+++ lib/roert.rb
@@ -0,0 +1,15 @@
+require 'halcyon'
+
+module Roert
+
+  def self.serve
+    puts "(Starting in #{Halcyon.root})"
+
+    Halcyon::Runner.load_config Halcyon.root/'config'/'config.yml'
+    Thin::Logging.silent = true if defined? Thin
+
+    require 'roert/controller'
+
+    @app = Halcyon::Runner.new
+  end
+end
