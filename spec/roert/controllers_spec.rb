df29f9fb9e14d5d6e1b28591e46c6695b5d3548d
Eivind Uggedal <eu@redflavor.com>
2008-04-09 15:01:04 +0200

    Moved spec to subdir for autotest to work.

--- spec/roert/controllers_spec.rb
+++ spec/roert/controllers_spec.rb
@@ -1,13 +1,13 @@
-require File.join(File.dirname(__FILE__), 'spec_helper')
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe 'controllers in general' do
 
   it 'should return a result' do
     res = Rack::MockRequest.new(@app).get('/greet')
-    res.body.should =~ /^\{"body":\{.+\}$/
+    res.body.should =~ /^\{"body":\{ "id.+\}$/
   end
 
-  it 'should return a result' do
+  it 'should return the correct content type' do
     res = Rack::MockRequest.new(@app).get('/greet')
     res.headers["Content-Type"].should =='application/json'
   end
