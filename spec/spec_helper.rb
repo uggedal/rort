8a6a6829170c5280220428f21263012cd49d0b1c
Eivind Uggedal <eu@redflavor.com>
2008-04-11 14:53:11 +0200

    Latest halcyon master does not set config on load_config.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ Spec::Runner.configure do |config|
   config.before(:all) do
     @log = ''
     @logger = Logger.new(StringIO.new(@log))
-    Halcyon::Runner.load_config Halcyon.root/'config'/'config.yml'
+    Halcyon.config = Halcyon::Runner.load_config
     Halcyon.config[:logger] = @logger
     Halcyon.config[:log_level] = 'debug'
     @app = Halcyon::Runner.new
