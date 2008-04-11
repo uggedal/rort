7bce637d44d967737d280beaf71e08bc4bec8ae6
Eivind Uggedal <eu@redflavor.com>
2008-04-11 19:46:49 +0200

    No need to fetch more than one artist in spec.

--- spec/roert/fetch_spec.rb
+++ spec/roert/fetch_spec.rb
@@ -35,8 +35,6 @@ end
 describe Roert::Fetch::Artist do
 
   it 'should provide the artists name' do
-    Fetch::Artist.as('uggedal').name.should == 'Eivind Uggedal'
-    Fetch::Artist.as('YTO').name.should == 'YTO'
     Fetch::Artist.as('TheMegaphonicThrift').
       name.should == 'The Megaphonic Thrift'
   end
