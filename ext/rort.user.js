42cc201fa87030f97f5c809096ee1453c0bd0d9c
Eivind Uggedal <eu@redflavor.com>
2008-04-18 18:06:06 +0200

    Jquery support on the client.

--- ext/rort.user.js
+++ ext/rort.user.js
@@ -7,6 +7,24 @@
 // @include        http://www11.nrk.no/urort/default.aspx
 // ==/UserScript==
 
+// Add jQuery
+var GM_JQ = document.createElement('script');
+GM_JQ.src = 'http://code.jquery.com/jquery-latest.pack.js';
+GM_JQ.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(GM_JQ);
+
+// Check if jQuery is loaded
+function GM_wait() {
+  if (typeof unsafeWindow.jQuery == 'undefined') {
+    window.setTimeout(GM_wait,100);
+  } else {
+    $ = unsafeWindow.jQuery; withJQuery();
+  }
+}
+
+GM_wait();
+
+// Cross-domain ajax get request with callback
 function get(url, callback) {
   GM_xmlhttpRequest({
     method: "GET",
@@ -14,11 +32,16 @@ function get(url, callback) {
      onload: function(xhr) { callback(xhr.responseText); }
   });
 }
- 
-function display(text) {
-  dummyDiv = document.createElement('div');
-  dummyDiv.innerHTML = text;
-  document.body.insertBefore(dummyDiv.firstChild, document.body.firstChild);
-}
 
-get('http://roert.redflavor.com/', display);
+// jQuery enabled scope
+function withJQuery() {
+
+  function display(text) {
+    $('#frontpage .mainnewsspot').prepend(text);
+  }
+
+  var user = $('ul#loggedinuser > li.item > a:first').attr('href');
+  display(user);
+
+  get('http://roert.redflavor.com/', display);
+}
