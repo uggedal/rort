731465c4d28d58cdfff74612cd1bec7cd4e49c8f
Eivind Uggedal <eu@redflavor.com>
2008-05-13 18:49:11 +0200

    Specs for the queue.

---
+++ spec/rort/queue_spec.rb
@@ -0,0 +1,41 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe Rort::Queue do
+
+  before(:each) do
+    Rort::Queue.clean
+  end
+
+  it 'should be able to push an item to the queue' do
+    Rort::Queue.push(345).should == [345]
+  end
+
+  it 'should be able to shift an item from the queue' do
+    Rort::Queue.push(345)
+    Rort::Queue.shift.should == 345
+  end
+
+  it 'pushing an item to the queue should increase the size' do
+    Rort::Queue.push(1).size.should == 1
+    (2..99).each do |i|
+      Rort::Queue.push(i)
+    end
+    Rort::Queue.push(100).size.should == 100
+  end
+
+  it 'shifting an item from the queue should decrease the size' do
+    (1..99).each do |i|
+      Rort::Queue.push(i)
+    end
+    Rort::Queue.push(100).size.should == 100
+
+    (1..50).each do |i|
+      Rort::Queue.shift.should == i
+    end
+    Rort::Queue.push(101).size.should == 51
+  end
+
+  it 'shifting when there are no items should return nil' do
+    Rort::Queue.shift.should be_nil
+  end
+end
