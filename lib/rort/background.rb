cd8a66385c00e5d592b11825df2cb76957f5d15e
Eivind Uggedal <eu@redflavor.com>
2008-05-13 19:43:19 +0200

    Added loggin support to bg.

--- lib/rort/background.rb
+++ lib/rort/background.rb
@@ -1,9 +1,19 @@
 $: << File.expand_path("../..", __FILE__)
 require 'rort'
 
+def log(msg)
+  start = Time.now
+  puts "#{start} | #{msg} | Starting..."
+  yield
+  stop = Time.now
+  puts "#{stop} | #{msg} | Finished in #{stop-start} sec"
+end
+
 loop do
   if slug = Rort::Queue.shift
-    Rort::Models::Person.fetch(slug).favorite_activities
+    log(slug) do
+      Rort::Models::Person.fetch(slug).favorite_activities
+    end
   end
   sleep 1
 end
